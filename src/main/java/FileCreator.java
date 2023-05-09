6797ae3c8ac7cb407625d24c644e50caa653ad97
e_j <etsku.jantola@gmail.com>
2023-05-09 15:48:51 +0300

    Added file creator file

--- src/main/java/FileCreator.java
+++ src/main/java/FileCreator.java
@@ -1,14 +1,13 @@
 package main.FileCreator;
+import java.Paths.FolderPaths;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import static java.Paths.FolderPaths.FILE_LOCATION;
 
-interface FILE_FOLDERS{
-    final String FILE_LOCATION = "C:\\Users\\etsku\\Desktop\\Java\\UserManagementSystem\\src\\main\\java\\DataFiles";
-}
 
-protected class create_file implements FILE_FOLDERS {
+protected class create_file implements FolderPaths {
 
     void create_new_file() throws IOException{
         Path file_path = Path.of(FILE_LOCATION);
