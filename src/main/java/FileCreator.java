6f0e38a7916dbc01befd02e6fceb7e12a34948ae
e_j <etsku.jantola@gmail.com>
2023-05-10 15:25:37 +0300

    Added input part to FileCreator file

--- src/main/java/FileCreator.java
+++ src/main/java/FileCreator.java
@@ -3,16 +3,34 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Scanner;
 
+import static java.Paths.FolderPaths.FILE_LOCATION;
 
-
+// GIT PULL FIRST TO GET PREVIOUS CHANGES FROM GITHUB, OTHERWISE GIT MIGHT HAVE PROBLEMS WHEN PUSHING TO REPOSITORY
 protected class create_file implements FILE_FOLDERS {
 
     void create_new_file() throws IOException{
+
+        // limit file size
+
+
+
+        // Create object to ask file name
+        Scanner file_name = new Scanner(System.in);
+        System.out.print("Enter new filename: ");
+        String filename = file_name.nextLine();
+        file_name.close();
+
+        // new files folder location part
         Path file_path = Path.of(FILE_LOCATION);
-        File new_file = new File(testFile.txt);
+        File new_file = new File(filename+".csv");
+        if(new_file.createNewFile()){
+            System.out.println("New file is created in DataFiles directory ");
+        } else System.out.println("Filename already exist");
         boolean created = new_file.createNewFile();
-        assertTrue(success);
+
+
 
     }
 
