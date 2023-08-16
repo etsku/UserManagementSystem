a858697f1065fe70da35949e7b6bc1b4d164b726
e_j <etsku.jantola@gmail.com>
2023-08-16 12:56:08 +0300

    Changes location of tests

---
+++ src/main/UserManagementApplication/Hibernate/Testing/UserInfoEntityTest.java
@@ -0,0 +1,13 @@
+package UserManagementApplication.Hibernate.Testing;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+
+public class UserInfoEntityTest {
+    @Test
+    // Accessibility, no null values
+    Hibernate.Entities test_information
+
+
+}
