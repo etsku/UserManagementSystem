4e7b8a82a52cddae848b4fa0fb03038d743e4208
e_j <etsku.jantola@gmail.com>
2023-08-23 12:44:31 +0300

    Mofified test file should be visible now

--- src/test/java/Testing/UserInfoEntityTest.java
+++ src/test/java/Testing/UserInfoEntityTest.java
@@ -1,5 +1,6 @@
 package Testing;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import Hibernate.Entities;
@@ -16,6 +17,32 @@ public class UserInfoEntityTest {
     @BeforeEach
     public void setUpObject(){
         test_entity = new Entities();
+    }
+    @Test
+    public void testSetName(){
+        test_entity.setName("Jack");
+        assertEquals("Jack",test_entity.getName());
+    }
+    @Test
+    public void testSetDepartment(){
+        test_entity.setDepartment("Finance");
+        assertEquals("Finance", test_entity.getDepartment());
+    }
+    @Test
+    public void testSetPosition(){
+        test_entity.setPosition("CFO");
+        assertEquals("CFO",test_entity.getPosition());
+    }
+    @Test
+    public void testShowUserInfo(){
+        test_entity.setName("Jack");
+        test_entity.setDepartment("IT");
+        test_entity.setPosition("Developer");
+        String expectedOutput =  "Employee ID: " + test_entity.getEmployeeID() + " Employee name: " + test_entity.getName() +
+                " Employee department: " + test_entity.getDepartment() +
+                " Employee position: " + test_entity.getPosition() + " ";
+        assertEquals(expectedOutput,test_entity.ShowUserInfo());
+
     }
 
 
