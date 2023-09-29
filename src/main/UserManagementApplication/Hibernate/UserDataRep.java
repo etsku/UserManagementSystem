7ced2030d997590f5eef28127ff9fa3dadf4fd8c
e_j <etsku.jantola@gmail.com>
2023-09-29 11:44:06 +0300

    Repository for database queries

--- src/main/UserManagementApplication/Hibernate/UserDataRep.java
+++ src/main/UserManagementApplication/Hibernate/UserDataRep.java
@@ -1,13 +1,19 @@
 package UserManagementApplication.Hibernate;
-import Hibernate.EmployeeEntities;
+import UserManagementApplication.Hibernate.EmployeeEntities;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+
 import org.springframework.stereotype.Repository;
 
 //  Greate new branch based on master branch to start develop this feature
+// Read about Spring Data JPA for this section
+// Repository is for data fetching operations
+// Also create service and controller to interact with repository (maybe using Spring)
 @Repository
 // Long can be replaced to other data types, primary key indicates what type should be used
-public interface userDataRep extends CrudRepository<EmployeeEntities,Integer>{
+public interface UserDataRep extends JpaRepository<EmployeeEntities,Integer> {
+    // JpaRepository gives more options for querying
+    // Add additional queries if needed, basic queries come from JpaRepository
+
 
 
 
